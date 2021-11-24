247147f53660f2645c4c27f066680c802bf64666
ferryagung <48131542+ferryagung@users.noreply.github.com>
2021-11-24 21:08:53 +0700

    Delete Prak6_WebDinamis directory

--- Prak6_WebDinamis/2/hapus_user.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "koneksi.php";
-    $sql="delete from user where id_user= '$_GET[id]'";
-    mysqli_query($con, $sql);
-    mysqli_close($con);
-    header('location:tampil_user.php');
-?>
